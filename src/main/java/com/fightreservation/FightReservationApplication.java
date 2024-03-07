5cfcd123153a18488029788991e865257e2d10be
Manish Indoor <Manish Indoor@LAPTOP-F2VHJ7C3>
2024-03-07 16:02:26 +0530

    This is my first commit

---
+++ src/main/java/com/fightreservation/FightReservationApplication.java
@@ -0,0 +1,13 @@
+package com.fightreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fightreservation/FightReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fightreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
